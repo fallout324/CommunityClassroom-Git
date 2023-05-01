d56f9eb3d6590e62b13a0c7f62fd516129c6830d
Michael WIsdom Song <msong226@uwo.ca>
2023-05-01 16:29:55 -0400

    Added python file

---
+++ trying.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello this is a test")
+    for i in range(5):
+        print("I'm trying to see how many times this repeats.")
+        new_dict = {"hello" : "12", "falsle" : "hello"}
+
+main()
